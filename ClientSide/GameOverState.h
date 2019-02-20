141c1aeac087b51262ce0a638cf5dd258ff5f84d
Unknown <izaa1109@gmail.com>
2019-02-20 19:17:07 +0100

    gameover state + HP

---
+++ ClientSide/GameOverState.h
@@ -0,0 +1,32 @@
+#include "BaseState.h"
+#include "StateManager.h"
+
+class GameOverState : public BaseState
+{
+public:
+	GameOverState(StateManager *stateManager);
+	~GameOverState(); 
+	
+	void onCreate() override;
+	void onDestroy() override;
+
+	void draw() override;
+	void update(const sf::Time &time) override;
+
+	void activate() override;
+	void deactivate() override;
+
+
+	void backToMenu(EventDetails *details);
+private:
+	sf::Sprite goSprite;
+	sf::Texture goTexture;
+
+	sf::Clock clock;
+	sf::Time time;
+
+	sf::IntRect rectSourceSprite;
+	unsigned int imageWidth;
+	unsigned int imageHeight;
+};
+
